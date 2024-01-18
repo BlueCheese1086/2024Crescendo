60dc5f98ecf7eb835c515b004307ef5aa170e310
teggos <teggorocks@gmail.com>
2024-01-18 18:39:36 -0500

    Initializing CAN motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,7 +7,7 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.DrivetrainConstants.DrivetrainLimits;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;

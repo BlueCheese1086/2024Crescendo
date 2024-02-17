ab1bea4240d3f8425ed3cda9d09567f7642e710e
turbojax07 <turbojax07@gmail.com>
2024-02-17 10:06:35 -0500

    Removing Swerve Drive from Kitbot (don't know why it was here)

--- src/main/java/frc/robot/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain/Drivetrain.java
@@ -1,24 +1,20 @@
 package frc.robot.Drivetrain;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix6.hardware.Pigeon2;
-
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.SwerveConstants;
 import frc.robot.Robot;
 
 public class Drivetrain extends SubsystemBase {
@@ -28,64 +24,58 @@ public class Drivetrain extends SubsystemBase {
     CANSparkMax frontRightMotor = new CANSparkMax(DriveConstants.FrontRightID, MotorType.kBrushless);
     CANSparkMax backRightMotor = new CANSparkMax(DriveConstants.BackRightID, MotorType.kBrushless);
 
-    // Swerve modules for the robot -- NOT BUILT
-    SwerveModule flModule = new SwerveModule("Front Left", SwerveConstants.frontLeftDriveID, SwerveConstants.frontLeftTurnID, SwerveConstants.frontLeftCancoderID, SwerveConstants.frontLeftOffset);
-    SwerveModule blModule = new SwerveModule("Back Left", SwerveConstants.backLeftDriveID, SwerveConstants.backLeftTurnID, SwerveConstants.backLeftCancoderID, SwerveConstants.backLeftOffset);
-    SwerveModule frModule = new SwerveModule("Front Right", SwerveConstants.frontRightDriveID, SwerveConstants.frontRightTurnID, SwerveConstants.frontRightCancoderID, SwerveConstants.frontRightOffset);
-    SwerveModule brModule = new SwerveModule("Back Right", SwerveConstants.backRightDriveID, SwerveConstants.backRightTurnID, SwerveConstants.backRightCancoderID, SwerveConstants.backRightOffset);
+    // Getting encoders for the primary (front) motors.
+    RelativeEncoder leftEncoder = frontLeftMotor.getEncoder();
+    RelativeEncoder rightEncoder = frontRightMotor.getEncoder();
 
     // Sensors
-    Pigeon2 gyro = new Pigeon2(DriveConstants.gyroID);
-
-    // Module Management
-    private final SwerveModule[] modules = {flModule, frModule, blModule, brModule};
-    private SwerveModuleState[] states = new SwerveModuleState[4];
-    private final SwerveModulePosition[] positions = new SwerveModulePosition[4];
+    AHRS gyro = new AHRS(Port.kMXP);
 
     // Kinematics
-    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(new Translation2d(SwerveConstants.kModuleToCenter, SwerveConstants.kModuleToCenter), new Translation2d(SwerveConstants.kModuleToCenter, -SwerveConstants.kModuleToCenter), new Translation2d(-SwerveConstants.kModuleToCenter, SwerveConstants.kModuleToCenter), new Translation2d(-SwerveConstants.kModuleToCenter, -SwerveConstants.kModuleToCenter));
-    private ChassisSpeeds speeds = new ChassisSpeeds(0, 0, 0);
+    private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(DriveConstants.kModuleToModuleDistance);
 
     // Odometry
-    private final SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, new Rotation2d(), positions);
+    private final Pose2d initPose = new Pose2d(2, 7, new Rotation2d());
+    private final DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(gyro.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition(), initPose);
     private final Field2d field = new Field2d();
-
-    // Timer
-    private final double start;
 
     /**
      * Constructor. This method is called when an instance of the class is created. This should generally be used to set up
      * instance variables and perform any configuration or necessary set up on hardware.
      */
     public Drivetrain() {
-        // Setting the start time
-        start = System.currentTimeMillis();
-
-        // Setting the default settings for each motor
+        // Restoring the default settings.
         frontLeftMotor.restoreFactoryDefaults();
-        frontLeftMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-        frontLeftMotor.setInverted(true);
-        frontLeftMotor.setIdleMode(IdleMode.kBrake);
-        frontLeftMotor.burnFlash();
-
         backLeftMotor.restoreFactoryDefaults();
-        backLeftMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-        backLeftMotor.setInverted(true);
-        backLeftMotor.setIdleMode(IdleMode.kBrake);
-        backLeftMotor.follow(frontLeftMotor);
-        backLeftMotor.burnFlash();
-
         frontRightMotor.restoreFactoryDefaults();
-        frontRightMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-        frontRightMotor.setInverted(false);
-        frontRightMotor.setIdleMode(IdleMode.kBrake);
-        frontRightMotor.burnFlash();
-
         backRightMotor.restoreFactoryDefaults();
+
+        // Setting a current limit.
+        frontLeftMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+        backLeftMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+        frontRightMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
         backRightMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-        backRightMotor.setInverted(false);
-        backRightMotor.setIdleMode(IdleMode.kBrake);
+
+        // Making the back motors follow the front.
+        backLeftMotor.follow(frontLeftMotor);
         backRightMotor.follow(frontRightMotor);
+
+        // Inverting motors as necessary.
+        frontLeftMotor.setInverted(true);
+        backLeftMotor.setInverted(true);
+        frontRightMotor.setInverted(false);
+        backRightMotor.setInverted(false);
+
+        // Setting the idle modes.
+        frontLeftMotor.setIdleMode(IdleMode.kBrake);
+        backLeftMotor.setIdleMode(IdleMode.kBrake);
+        frontRightMotor.setIdleMode(IdleMode.kBrake);
+        backRightMotor.setIdleMode(IdleMode.kBrake);
+
+        // Saving the settings
+        frontLeftMotor.burnFlash();
+        backLeftMotor.burnFlash();
+        frontRightMotor.burnFlash();
         backRightMotor.burnFlash();
     }
 
@@ -95,36 +85,7 @@ public class Drivetrain extends SubsystemBase {
      * 
      */
     @Override
-    public void periodic() {
-
-    }
-
-    /**
-     * Yet to be implemented -- This is theoretical code for when Kitbot gets the swerve upgrade.
-     * Drives the robot in different directions determined by the parameters.
-     * 
-     * @param xTraverse
-     * @param zTraverse
-     * @param zRotate
-     */
-    public void swerveDrive(ChassisSpeeds speeds) {
-        // Updating the local speeds variable
-        this.speeds = speeds;
-
-        // Converting the ChassisSpeeds to SwerveModuleStates
-        states = kinematics.toSwerveModuleStates(speeds);
-
-        // Setting the states of each module
-        flModule.setState(states[0]);
-        blModule.setState(states[1]);
-        frModule.setState(states[2]);
-        brModule.setState(states[3]);
-
-        // Updating the Gyro if the robot is being simulated
-        if (Robot.isSimulation()) {
-            gyro.setYaw(gyro.getYaw().getValueAsDouble() + 180 + (System.currentTimeMillis() - start) / 1000.0 * Units.radiansToDegrees(speeds.omegaRadiansPerSecond));
-        }
-    }
+    public void periodic() {}
 
     /**
      * Drives the robot at the desired forward speed combined with the rotational speed.

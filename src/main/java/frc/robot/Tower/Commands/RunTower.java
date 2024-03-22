2fc50a40aa56922f2a3e9bab0bc1ebca813bfc5e
turbojax07 <turbojax07@gmail.com>
2024-03-22 17:08:36 -0400

    Forgot to change the Tower commands for the last commit.

--- src/main/java/frc/robot/Tower/Commands/RunTower.java
+++ src/main/java/frc/robot/Tower/Commands/RunTower.java
@@ -3,18 +3,18 @@ package frc.robot.Tower.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Tower.Tower;
 
-public class RunLeftTower extends Command {
+public class RunTower extends Command {
     private Tower tower;
     private double speed;
 
     /**
-     * Creates a new RunLeftTower Command.
+     * Creates a new RunTower Command.
      * <p>
-     * This command runs the left tower of the robot at the given speed.
+     * This command runs the tower of the robot at the given speed.
      * 
-     * @param speed The percent speed to run the left tower at.
+     * @param speed The percent speed to run the tower at.
      */
-    public RunLeftTower(double speed) {
+    public RunTower(double speed) {
         this.tower = Tower.getInstance();
         this.speed = speed;
     }
@@ -22,12 +22,12 @@ public class RunLeftTower extends Command {
     /** This function runs every 20 ms while the command is scheduled. */
     @Override
     public void execute() {
-        tower.setLeftSpeed(speed);
+        tower.setSpeed(speed);
     }
 
     /** This function runs once when the command ends. */
     @Override
     public void end(boolean interrupted) {
-        tower.setLeftSpeed(0);
+        tower.setSpeed(0);
     }
 }

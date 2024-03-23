702b16ecfecc6a82b678abb4e8ed1e579bb312be
turbojax07 <turbojax07@gmail.com>
2024-03-22 20:43:40 -0400

    Binding controls and adding a reference photo.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 import frc.robot.Drivetrain.Commands.*;
 import frc.robot.Drivetrain.Drivetrain;
@@ -17,7 +19,6 @@ import frc.robot.Shooter.Commands.*;
 import frc.robot.Shooter.Shooter;
 import frc.robot.Tower.Commands.*;
 import frc.robot.Tower.Tower;
-import frc.robot.Vision.Commands.*;
 import frc.robot.Vision.Vision;
 
 public class RobotContainer {
@@ -36,17 +37,42 @@ public class RobotContainer {
         configureBindings();
     }
 
-    private void configureBindings() {}
+    private void configureBindings() {
+        /*  All operator controls. -- See Controls.png for visual reference.
+            A: Runs the feed
+            B: Runs the intake
+            X: Runs the launcher in reverse
+            Y: Toggles the launcher
+            Left Bumper: Moves the shooter up.
+            Left Trigger: Moves the shooter down.
+            Right Bumper: Moves the tower up.
+            Right Trigger: Moves the tower down.
+            Back: Running the intake in reverse.
+            POV Up: Stowing the intake.
+            POV Down: Extending the intake.
+        */
+        operatorController.a().whileTrue(new SetFeedSpeed(1));
+        operatorController.b().whileTrue(new SetIntakeSpeed(1));
+        operatorController.x().whileTrue(new SetLauncherSpeed(-1));
+        operatorController.y().toggleOnTrue(new SetLauncherSpeed(1));
+        operatorController.leftBumper().whileTrue(new SetShooterAngle(shooter.getAngle(), 1));
+        operatorController.leftTrigger().whileTrue(new SetShooterAngle(shooter.getAngle(), -1));
+        operatorController.rightBumper().whileTrue(new SetTowerSpeed(1));
+        operatorController.rightTrigger().whileTrue(new SetTowerSpeed(-1));
+        operatorController.back().whileTrue(new SetIntakeSpeed(-1));
+        operatorController.povUp().onTrue(new SetIntakeAngle(Rotation2d.fromDegrees(90)));
+        operatorController.povDown().onTrue(new SetIntakeAngle(new Rotation2d()));
+    }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return new PathPlannerAuto("v3-auto");
     }
 
     public Command getTeleopCommand() {
         return new SwerveDrive(drivetrain, 
-                               () -> MathUtil.applyDeadband(driveController.getLeftX(), 0.2),
-                               () -> MathUtil.applyDeadband(driveController.getLeftY(), 0.2),
-                               () -> MathUtil.applyDeadband(driveController.getRightX(), 0.2)
+            () -> MathUtil.applyDeadband(driveController.getLeftX(), 0.2),
+            () -> MathUtil.applyDeadband(driveController.getLeftY(), 0.2),
+            () -> MathUtil.applyDeadband(driveController.getRightX(), 0.2)
         );
     }
 }

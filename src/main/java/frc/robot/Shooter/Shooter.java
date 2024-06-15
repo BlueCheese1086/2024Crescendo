2398c9edf24ea43265dc5d6ed8ef06a2a86da0d7
turbojax07 <turbojax07@gmail.com>
2024-06-15 09:27:41 -0400

    Replacing talons on the shooter.

--- src/main/java/frc/robot/Shooter/Shooter.java
+++ src/main/java/frc/robot/Shooter/Shooter.java
@@ -1,9 +1,5 @@
 package frc.robot.Shooter;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
@@ -11,50 +7,13 @@ import com.revrobotics.CANSparkMax;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter {
-    // motors
-    // this code was brought to you by lil' python
-    private TalonFX lShooter = new TalonFX(ShooterConstants.lShooterID);
-    private TalonFX rShooter = new TalonFX(ShooterConstants.rShooterID);
-
+    // Motors
+    private CANSparkMax lShooter = new CANSparkMax(ShooterConstants.lShooterID, MotorType.kBrushless);
+    private CANSparkMax rShooter = new CANSparkMax(ShooterConstants.rShooterID, MotorType.kBrushless);
     private CANSparkMax feedRoller = new CANSparkMax(ShooterConstants.feedRollerID, MotorType.kBrushless);
 
     // A common instance of the shooter subsystem.
     private static Shooter instance;
-
-    //🫵🤫
-    public enum State {
-        IDLE,
-        SPINNINGUPANDFEEDING,
-        SHOOTING,
-        SPINNINGUPNOFEEDING,
-        WEAKSHOT
-    }
-
-    public Shooter() {
-        // Creating the configuration objects for the talons
-        TalonFXConfiguration lShooterConfig = new TalonFXConfiguration();
-        TalonFXConfiguration rShooterConfig = new TalonFXConfiguration();
-
-        // Setting the idle mode of the talons
-        lShooterConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        rShooterConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-
-        // Updating the settings of the talons
-        lShooter.getConfigurator().apply(lShooterConfig);
-        rShooter.getConfigurator().apply(rShooterConfig);
-
-        // Making the rShooter motor follow the lShooter motor
-        rShooter.setControl(new Follower(lShooter.getDeviceID(), true));
-
-        // Resetting the settings of the sparkmaxes
-        feedRoller.restoreFactoryDefaults();
-
-        // Setting the idle mode of the sparkmaxes
-        feedRoller.setIdleMode(IdleMode.kBrake);
-
-        // Saving the settings of the sparkmaxes
-        feedRoller.burnFlash();
-    }
 
     /**
      * This function gets a common instance of the shooter subsystem that anyone can access.
@@ -68,6 +27,26 @@ public class Shooter {
         if (instance == null) instance = new Shooter();
 
         return instance;
+    }
+
+    public Shooter() {
+        // Resetting the settings of the sparkmaxes
+        lShooter.restoreFactoryDefaults();
+        rShooter.restoreFactoryDefaults();
+        feedRoller.restoreFactoryDefaults();
+
+        // Setting the idle mode of the sparkmaxes
+        lShooter.setIdleMode(IdleMode.kBrake);
+        rShooter.setIdleMode(IdleMode.kBrake);
+        feedRoller.setIdleMode(IdleMode.kBrake);
+
+        // Making the rShooter motor follow the lShooter motor
+        rShooter.follow(lShooter, true);
+
+        // Saving the settings of the sparkmaxes
+        lShooter.burnFlash();
+        rShooter.burnFlash();
+        feedRoller.burnFlash();
     }
 
     /**

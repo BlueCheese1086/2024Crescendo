d9c8ddbea59a8e8d30f30359547a1047b29933ec
Turbojax07 <turbojax07@gmail.com>
2024-07-31 00:05:04 -0700

    Starting on a Vision subsystem

---
+++ src/main/java/frc/robot/Vision/Vision.java
@@ -0,0 +1,92 @@
+// package frc.robot.Vision;
+
+// import java.util.List;
+
+// import org.photonvision.PhotonCamera;
+// import org.photonvision.PhotonPoseEstimator;
+// import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+// import org.photonvision.targeting.PhotonPipelineResult;
+// import org.photonvision.targeting.PhotonTrackedTarget;
+
+// import edu.wpi.first.apriltag.AprilTagFieldLayout;
+// import edu.wpi.first.apriltag.AprilTagFields;
+// import edu.wpi.first.math.geometry.Rotation3d;
+// import edu.wpi.first.math.geometry.Transform3d;
+// import edu.wpi.first.math.geometry.Translation3d;
+// import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.DriverStation.Alliance;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// public class Vision extends SubsystemBase {
+//     private PhotonCamera cam1;
+//     private PhotonCamera cam2;
+
+//     private Transform3d robotToCam1 = new Transform3d(new Translation3d(), new Rotation3d());
+//     private Transform3d robotToCam2 = new Transform3d(new Translation3d(), new Rotation3d());
+
+//     private PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(AprilTagFields.k2024Crescendo.loadAprilTagLayoutField(), PoseStrategy.CLOSEST_TO_REFERENCE_POSE, robotToCam1)
+//     public Vision() {
+//         cam1 = new PhotonCamera("FL_Camera");
+//         cam2 = new PhotonCamera("FR_Camera");
+//     }
+
+//     public void getResults() {
+//         PhotonPipelineResult result1 = cam1.getLatestResult();
+//         PhotonPipelineResult result2 = cam2.getLatestResult();
+
+//         if (result1.hasTargets()) {
+//             List<PhotonTrackedTarget> targets = result1.targets;
+
+//             for (PhotonTrackedTarget target : targets) {
+//                 boolean onBlue = DriverStation.getAlliance().orElse(Alliance.Blue).equals(Alliance.Blue);
+
+//                 if (onBlue) {
+//                     switch (target.getFiducialId()) {
+
+//                     }
+//                 } else {
+
+//                 }
+
+//                 switch(target.getFiducialId()) {
+//                     case 1:
+//                         break;
+//                     case 2:
+//                         break;
+//                     case 3:
+//                         break;
+//                     case 4:
+//                         break;
+//                     case 5:
+//                         break;
+//                     case 6:
+//                         break;
+//                     case 7:
+//                         break;
+//                     case 8:
+//                         break;
+//                     case 9:
+//                         break;
+//                     case 10:
+//                         break;
+//                     case 11:
+//                         break;
+//                     case 12:
+//                         break;
+//                     case 13:
+//                         break;
+//                     case 14:
+//                         break;
+//                     case 15:
+//                         break;
+//                     case 16:
+//                         break;
+//                 }
+//             }
+//         }
+
+//         if (result2.hasTargets()) {
+//             List<PhotonTrackedTarget> targets = result2.targets;
+//         }
+//     }
+// }

331d595d3a1d26ba113a17089ff441784c0c6ec1
AhAcKeR9207 <turbojax07@gmail.com>
2024-08-03 04:25:34 -0400

    implementing closed loop controls

--- src/main/java/frc/robot/Drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain/Drivetrain.java
@@ -5,62 +5,179 @@
 package frc.robot.Drivetrain;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.ReplanningConfig;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class Drivetrain extends SubsystemBase {
-    CANSparkMax leftMotor;
-    CANSparkMax leftBackMotor;
-    CANSparkMax rightMotor;
-    CANSparkMax rightBackMotor;
+    // Motors
+    private CANSparkMax flMotor;
+    private CANSparkMax frMotor;
+    private CANSparkMax blMotor;
+    private CANSparkMax brMotor;
 
-    SparkPIDController leftPID;
-    SparkPIDController rightPID;
+    // Encoders
+    private RelativeEncoder leftEncoder;
+    private RelativeEncoder rightEncoder;
+
+    // PID Controllers
+    private SparkPIDController leftPID;
+    private SparkPIDController rightPID;
+
+    // Gyro
+    private PigeonIMU gyro;
+
+    // Kinematics
+    private DifferentialDriveKinematics kinematics;
+
+    // Odometry
+    private Pose2d initPose = new Pose2d();
+    private DifferentialDrivePoseEstimator poseEstimator;
+
+    private static Drivetrain instance;
+
+    public static Drivetrain getInstance() {
+        if (instance == null) instance = new Drivetrain();
+
+        return instance;
+    }
 
     /** Creates a new ExampleSubsystem. */
     public Drivetrain() {
+        // Getting motors
+        flMotor = new CANSparkMax(DriveConstants.frontLeftID, MotorType.kBrushless);
+        frMotor = new CANSparkMax(DriveConstants.frontRightID, MotorType.kBrushless);
+        blMotor = new CANSparkMax(DriveConstants.backLeftID, MotorType.kBrushless);
+        brMotor = new CANSparkMax(DriveConstants.backRightID, MotorType.kBrushless);
 
-        leftMotor = new CANSparkMax(DriveConstants.frontLeftID, MotorType.kBrushless);
-        leftBackMotor = new CANSparkMax(DriveConstants.backLeftID, MotorType.kBrushless);
-        rightMotor = new CANSparkMax(DriveConstants.frontRightID, MotorType.kBrushless);
-        rightBackMotor = new CANSparkMax(DriveConstants.backRightID, MotorType.kBrushless);
+        // Resetting motors
+        flMotor.restoreFactoryDefaults();
+        frMotor.restoreFactoryDefaults();
+        blMotor.restoreFactoryDefaults();
+        brMotor.restoreFactoryDefaults();
 
-        rightBackMotor.restoreFactoryDefaults();
-        leftBackMotor.restoreFactoryDefaults();
-        rightMotor.restoreFactoryDefaults();
-        leftMotor.restoreFactoryDefaults();
+        // Creating followers
+        blMotor.follow(flMotor);
+        brMotor.follow(frMotor);
 
-        leftBackMotor.follow(leftMotor);
-        rightBackMotor.follow(rightMotor);
+        // Inverting motors
+        blMotor.setInverted(true);
+        brMotor.setInverted(false);
+        flMotor.setInverted(true);
+        frMotor.setInverted(false);
 
-        leftBackMotor.setInverted(true);
-        rightBackMotor.setInverted(false);
-        leftMotor.setInverted(true);
-        rightMotor.setInverted(false);
+        // Setting idle modes
+        flMotor.setIdleMode(IdleMode.kBrake);
+        blMotor.setIdleMode(IdleMode.kBrake);
+        frMotor.setIdleMode(IdleMode.kBrake);
+        brMotor.setIdleMode(IdleMode.kBrake);
 
-        leftMotor.setIdleMode(IdleMode.kBrake);
-        leftBackMotor.setIdleMode(IdleMode.kBrake);
-        rightMotor.setIdleMode(IdleMode.kBrake);
-        rightBackMotor.setIdleMode(IdleMode.kBrake);
+        // Getting encoders
+        leftEncoder = flMotor.getEncoder();
+        rightEncoder = frMotor.getEncoder();
 
-        rightBackMotor.burnFlash();
-        leftBackMotor.burnFlash();
-        rightMotor.burnFlash();
-        leftMotor.burnFlash();
+        // Getting gyro
+        gyro = new PigeonIMU(10);
+        gyro.setYaw(0);
 
+        // Setting conversion factors
+        leftEncoder.setPositionConversionFactor(DriveConstants.posConversionFactor);
+        leftEncoder.setVelocityConversionFactor(DriveConstants.velConversionFactor);
+        rightEncoder.setPositionConversionFactor(DriveConstants.posConversionFactor);
+        rightEncoder.setVelocityConversionFactor(DriveConstants.velConversionFactor);
+
+        // Getting PID controllers
+        leftPID = flMotor.getPIDController();
+        rightPID = frMotor.getPIDController();
+
+        // Saving configs
+        brMotor.burnFlash();
+        blMotor.burnFlash();
+        frMotor.burnFlash();
+        flMotor.burnFlash();
+
+        // Initializing the kinematics
+        kinematics = new DifferentialDriveKinematics(DriveConstants.robotWidth);
+
+        // Initializing the pose estimator
+        poseEstimator = new DifferentialDrivePoseEstimator(kinematics, getAngle(), getLeftPosition(), getRightPosition(), initPose);
+
+        // Initializing the autobuilder
+        AutoBuilder.configureRamsete(
+            this::getPose,
+            this::setPose,
+            this::getSpeeds,
+            this::closedLoop,
+            new ReplanningConfig(),
+            () -> (DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Red),
+            this
+        );
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("/Drivetrain/Real_Speed_F/S", 0);
+
+
+        SmartDashboard.putNumber("/Drivetrain/Left_Actual_MPS", 0);
+        SmartDashboard.putNumber("/Drivetrain/Left_Expected_MPS", 0);
+        SmartDashboard.putNumber("/Drivetrain/Right_Actual_MPS", 0);
+        SmartDashboard.putNumber("/Drivetrain/Right_Expected_MPS", 0);
+        SmartDashboard.putNumber("/Drivetrain/Angle", getAngle().getRadians());
     }
-        
-    public void drive(double xSpeed, double zRotate) {
+
+    public Rotation2d getAngle() {
+        return Rotation2d.fromDegrees(gyro.getYaw());
+    }
+    public Pose2d getPose() {
+        return poseEstimator.getEstimatedPosition();
+    }
+
+    public void setPose(Pose2d newPose) {
+        poseEstimator.resetPosition(getAngle(), getPosition(), newPose);
+    }
+
+    public ChassisSpeeds getSpeeds() {
+        return kinematics.toChassisSpeeds(new DifferentialDriveWheelSpeeds(getLeftVelocity(), getRightVelocity()));
+    }
+
+    public DifferentialDriveWheelPositions getPosition() {
+        return new DifferentialDriveWheelPositions(getLeftPosition(), getRightPosition());
+    }
+
+    public double getLeftPosition() {
+        return leftEncoder.getPosition();
+    }
+
+    public double getLeftVelocity() {
+        return leftEncoder.getVelocity();
+    }
+
+    public double getRightPosition() {
+        return rightEncoder.getPosition();
+    }
+
+    public double getRightVelocity() {
+        return rightEncoder.getVelocity();
+    }
+    
+    public void arcadeDrive(double xSpeed, double zRotate) {
         xSpeed *= DriveConstants.maxDriveSpeed;
         zRotate *= DriveConstants.maxTurnSpeed;
 
@@ -72,7 +189,30 @@ public class Drivetrain extends SubsystemBase {
         if (zRotate != 0 && zRotate > 0) zRotate -= 0.1;
         if (zRotate != 0 && zRotate < 0) zRotate += 0.1;
 
-        leftMotor.set(xSpeed - zRotate);
-        rightMotor.set(xSpeed + zRotate);
+        flMotor.set(xSpeed - zRotate);
+        frMotor.set(xSpeed + zRotate);
+    }
+
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+        leftSpeed *= DriveConstants.maxDriveSpeed;
+        rightSpeed *= DriveConstants.maxTurnSpeed;
+
+        if (leftSpeed < 0.1 && leftSpeed > -0.1) leftSpeed = 0;
+        if (rightSpeed < 0.1 && rightSpeed > -0.1) rightSpeed = 0;
+
+        if (leftSpeed  != 0 && leftSpeed  > 0) leftSpeed  -= 0.1;
+        if (leftSpeed  != 0 && leftSpeed  < 0) leftSpeed  += 0.1;
+        if (rightSpeed != 0 && rightSpeed > 0) rightSpeed -= 0.1;
+        if (rightSpeed != 0 && rightSpeed < 0) rightSpeed += 0.1;
+
+        flMotor.set(leftSpeed);
+        frMotor.set(rightSpeed);
+    }
+
+    public void closedLoop(ChassisSpeeds speeds) {
+        DifferentialDriveWheelSpeeds newSpeeds = kinematics.toWheelSpeeds(speeds);
+
+        leftPID.setReference(newSpeeds.leftMetersPerSecond, ControlType.kVelocity);
+        rightPID.setReference(newSpeeds.rightMetersPerSecond, ControlType.kVelocity);
     }
 }
--- src/main/java/frc/robot/Shooter/Shooter.java
+++ src/main/java/frc/robot/Shooter/Shooter.java
@@ -2,41 +2,68 @@ package frc.robot.Shooter;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
-    private CANSparkMax launcher;
-    private CANSparkMax feeder;
+    // Motors
+    private CANSparkMax launcherMotor;
+    private CANSparkMax feederMotor;
 
+    // Encoders
     private RelativeEncoder launcherEncoder;
     private RelativeEncoder feederEncoder;
 
+    // PID Controllers
+    private SparkPIDController launcherPID;
+    private SparkPIDController feederPID;
+
+    // Logged Vars
     private double expectedFeedSpeed;
     private double expectedLaunchSpeed;
 
+    private static Shooter instance;
+
+    public static Shooter getInstance() {
+        if (instance == null) instance = new Shooter();
+
+        return instance;
+    }
+
     public Shooter() {
-        launcher = new CANSparkMax(ShooterConstants.launchID, MotorType.kBrushless);
-        feeder = new CANSparkMax(ShooterConstants.feedID, MotorType.kBrushless);
+        // Getting motors
+        launcherMotor = new CANSparkMax(ShooterConstants.launchID, MotorType.kBrushless);
+        feederMotor = new CANSparkMax(ShooterConstants.feedID, MotorType.kBrushless);
 
-        launcher.restoreFactoryDefaults();
-        feeder.restoreFactoryDefaults();
+        // Resetting motor configs
+        launcherMotor.restoreFactoryDefaults();
+        feederMotor.restoreFactoryDefaults();
 
-        launcher.setIdleMode(IdleMode.kCoast);
-        feeder.setIdleMode(IdleMode.kCoast);
+        // Setting the idle mode of motors
+        launcherMotor.setIdleMode(IdleMode.kCoast);
+        feederMotor.setIdleMode(IdleMode.kCoast);
 
-        launcher.setInverted(true);
-        feeder.setInverted(true);
+        // Inverting the motors
+        launcherMotor.setInverted(true);
+        feederMotor.setInverted(true);
 
-        launcherEncoder = launcher.getEncoder();
-        feederEncoder = feeder.getEncoder();
+        // Getting the encoders
+        launcherEncoder = launcherMotor.getEncoder();
+        feederEncoder = feederMotor.getEncoder();
 
-        launcher.burnFlash();
-        feeder.burnFlash();
+        // Getting the PID controllers
+        launcherPID = launcherMotor.getPIDController();
+        feederPID = feederMotor.getPIDController();
+
+        // Saving motor configs
+        launcherMotor.burnFlash();
+        feederMotor.burnFlash();
     }
 
     @Override
@@ -58,11 +85,11 @@ public class Shooter extends SubsystemBase {
 
     public void setFeedSpeed(double speed) {
         expectedFeedSpeed = speed;
-        feeder.set(speed * ShooterConstants.maxFeedSpeed);
+        feederMotor.set(speed * ShooterConstants.maxFeedSpeed);
     }
 
     public void setLaunchSpeed(double speed) {
         expectedLaunchSpeed = speed;
-        launcher.set(speed * ShooterConstants.maxLaunchSpeed);
+        launcherMotor.set(speed * ShooterConstants.maxLaunchSpeed);
     }
 }

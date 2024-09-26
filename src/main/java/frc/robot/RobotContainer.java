5bdf5d5fc6051383a32775cbed5597455fcbf3e2
turbojax07 <turbojax07@gmail.com>
2024-09-26 18:07:43 -0400

    Removing unused inputs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,8 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Drivetrain.Commands.*;
 import frc.robot.Drivetrain.Drivetrain;
